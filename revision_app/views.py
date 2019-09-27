02751f80ef476929e6e8f9f7c74a86b3aebcc2de
benwaz78 <benwaz78@gmail.com>
2019-09-27 11:01:55 +0100

    My commit

---
+++ revision_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from revision_app.models import About, Service
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from revision_app.forms import RegForm
+
+# Create your views here.
+
+def index(request):
+	show_service = Service.objects.all()
+
+	show_about = About.objects.all()
+
+	home_dict = {
+		'serv_key':show_service,
+		'abt_key':show_about
+	}
+
+	return render(request, 'front_end/index.html', context=home_dict)
+
+def about(request):
+	list_abt = About.objects.all()
+	return render(request, 'front_end/about.html', {'abt_list':list_abt})
+
+def users(request):
+	list_usr = User.objects.all()
+	return render(request, 'front_end/users.html', {'usr_list':list_usr})
+
+def service(request):
+	list_srv = Service.objects.all()
+	return render(request, 'front_end/services.html', {'srv_list':list_srv})
+
+def about_detail(request, abt_id):
+	about_detail = About.objects.get(pk=abt_id)
+	return render(request, 'front_end/detail.html', {'abt_det':about_detail})
+
+
+def dashboard(request):
+	return render(request, 'front_end/dashboard.html', {'mydbt':'Dashboard'})
+
+
+def register(request):
+	if request.method == 'POST':
+		register_form = RegForm(request.POST)
+		if register_form.is_valid():
+			register_form.save()
+			return redirect('revision_app:login')
+	else:
+		register_form = RegForm()
+	return render(request, 'registration/register.html', {'reg':register_form})
+
+
