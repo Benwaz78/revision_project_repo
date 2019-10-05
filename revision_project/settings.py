ca1652ff0e5000b3a3f414e70fa75368f70e4a59
benwaz78 <benwaz78@gmail.com>
2019-10-05 19:48:51 +0100

    I push this here

--- revision_project/settings.py
+++ revision_project/settings.py
@@ -84,7 +84,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
